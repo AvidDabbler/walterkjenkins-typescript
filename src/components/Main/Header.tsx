225d291a38af5841a806db13b7e4a02d1924fe52
AvidDabbler <walter.k.jenkins@gmail.com>
2022-01-02 11:14:36 -0600

    added tailwind

--- src/components/Main/Header.tsx
+++ src/components/Main/Header.tsx
@@ -1,6 +1,7 @@
 export const Header = () => {
 	return (
-		<div id="head" className="op80 w-100 zi100 pa0 inline w-100 pv2 v-mid">
+		// <div id="head" className="op80 w-100 zi100 pa0 inline w-100 pv2 v-mid">
+		<div id="head" className="w-full flex justify-between">
 			<nav className="pv3 ph2 tc fr ph3">
 				<a className="link mh3 f6" data-value="services" href="#services">
 					Services
