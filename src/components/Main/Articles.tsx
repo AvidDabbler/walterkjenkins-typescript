474eb296b89f06340a5bb787b4ea457e211a9342
aviddabbler <walter.k.jenkins@gmail.com>
2022-01-23 14:20:41 -0600

    cleanup

--- src/components/Main/Articles.tsx
+++ src/components/Main/Articles.tsx
@@ -8,11 +8,11 @@ import { Header } from ".";
 export function Articles() {
 	const { articles, setArticles } = useContext(appContext);
 	const getArticles = async () => {
-		const request = await fetch(
+		let request = await fetch(
 			"https://notion-api.splitbee.io/v1/table/beba8785230d43b3ab8916d63ae0e360",
 		).then((res) => res.json());
+		request = request.filter((item: any) => item.published)
 		setArticles(request);
-		console.log(request);
 	};
 
 	useEffect(() => {
@@ -28,8 +28,8 @@ export function Articles() {
 			<div className="flex flex-col w-full h-full min-h-screen bg-blue topo items-center pt-14 text-white mp-14">
 				<div className="my-14">
 					<h1 className="text-3xl">Blog</h1>
-					<div className="w-full bg-orange h-14">
-						<Link to="/free-lessons">Free Lessons</Link>
+					<div className="w-1/2 text-2xl signup rounded-full flex justify-center m-auto opacity-100 text-center items-center bg-orange my-auto">
+						<Link className="items-center my-auto" to="/free-lessons">Free Lessons</Link>
 					</div>
 					{articles.length > 0
 						? articles.map((el: LessonType) => <Article article={el} />)
--- src/components/Main/Lessons.tsx
+++ src/components/Main/Lessons.tsx
@@ -1,38 +1,12 @@
 import React, { useEffect, useContext, useState } from "react";
 import { appContext } from "../../context";
-import { Article } from ".";
 import { LessonType } from "../../types";
-import makeAnimated from "react-select/animated";
-
-const initFilters = {
-	tags: [],
-	path: [],
-	technology: [],
-};
+import { Lesson } from "./Lesson";
 
 export function Lessons() {
 	const [displayedLessons, setDisplayedLessons] = useState([]);
 
 	const { setLessons } = useContext(appContext);
-
-	// const reduceList = (key: string, request: Array<any>) => {
-	// 	let list = request.reduce((acc: any, cur) => {
-	// 		for (let tag of cur[key]) {
-	// 			// const value = { value: tag, label: tag, isFixed: true }
-	// 			if (!acc.includes(tag)) {
-	// 				acc = [...acc, tag];
-	// 			}
-	// 		}
-	// 		return acc;
-	// 	}, []);
-	// 	list.sort((a: string, b: string) => {
-	// 		if (a > b) return 1;
-	// 		else return -1;
-	// 	});
-	// 	return list.map((tag: string) => {
-	// 		return { value: tag, label: tag, color: "#FF5630", isFixed: true };
-	// 	});
-	// };
 
 	const getLessons = async () => {
 		const request = await fetch(
@@ -40,11 +14,6 @@ export function Lessons() {
 		).then((res) => res.json());
 		setLessons(request);
 
-		// const tags = reduceList("tags", request);
-		// const path = reduceList("path", request);
-		// const technology = reduceList("technology", request);
-
-		// setFilters({ tags, path, technology });
 		setDisplayedLessons(request);
 	};
 
@@ -53,76 +22,17 @@ export function Lessons() {
 		// eslint-disable-next-line
 	}, []);
 
-	// useEffect(() => {
-	// 	updateFilters();
-	// }, [activeFilters]);
-
 	return (
 		<div className="my-auto h-full">
 			<div className="bg-orange h-px"></div>
 			<div className="px-4 flex flex-col w-full h-full bg-blue topo min-h-screen items-center pt-14 text-white mp-14">
 				<div className="my-14">
 					<h1 className="text-3xl">Free Lessons</h1>
-					{/* <div className="flex ml-auto px-4">
-						<button
-							className="ml-auto"
-							onClick={() => setRevealFilters(!revealFilters)}>
-							<img src={Filter} alt="filter lessons"></img>
-						</button>
-					</div>
-					<div className="flex flex-wrap ">
-						{filters.tags.length > 1 && (
-							<div
-								className={`text-black w-80 px-2 ${
-									revealFilters ? "" : "hidden"
-								}`}>
-								<span className="text-white">Tags</span>
-								<Select
-									value={activeFilters.tags}
-									onChange={(selected: any) => changeDropDown(selected, "tags")}
-									className="basic-multi-select bg-green-400 rounded-lg"
-									isMulti
-									options={filters.tags}
-								/>
-							</div>
-						)}
-						{filters.path.length > 1 && (
-							<div
-								className={`text-black w-80 px-2 ${
-									revealFilters ? "" : "hidden"
-								}`}>
-								<span className="text-white">Path</span>
-								<Select
-									value={activeFilters.path}
-									onChange={(selected: any) => changeDropDown(selected, "path")}
-									className="bg-green-400 rounded-lg"
-									options={filters.path}
-								/>
-							</div>
-						)}
-						{filters.technology.length > 1 && (
-							<div
-								className={`text-black w-80 px-2 ${
-									revealFilters ? "" : "hidden"
-								}`}>
-								<span className="text-white">Technology</span>
-								<Select
-									isSearchable
-									value={activeFilters.technology}
-									onChange={(selected: any) =>
-										changeDropDown(selected, "technology")
-									}
-									className="bg-green-400 rounded-lg"
-									isMulti
-									options={filters.technology}
-								/>
-							</div>
-						)}
-					</div> */}
+
 					<div className="">
 						{displayedLessons.length > 0
 							? displayedLessons.map((el: LessonType) => (
-									<Article article={el} />
+									<Lesson article={el} />
 							  ))
 							: "loading..."}
 					</div>
